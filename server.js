e7eaf8f8a6dc3f22fe14a91a1279ec9eb867150a
Jsheen98 <jsheendev@gmail.com>
2023-03-03 18:40:19 -0700

    Added some functionality for viewing roles

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Calls required dependencies
 const mysql = require('mysql2')
 const cTable = require('console.table');
 const inquirer = require('inquirer')
 
+// Connects with the mysql database created from schema and seed files
 const employeeDb = mysql.createConnection(
     {
         host: 'localhost',
@@ -11,6 +13,7 @@ const employeeDb = mysql.createConnection(
     }
 )
 
+// Starting user prompt
 function startTracker() {
     inquirer.prompt([
         {
@@ -28,20 +31,45 @@ function startTracker() {
             ]
         }
     ])
+    // Function goes through and selects the function option based off the user selection
     .then(function selections(data) {
         switch (data.options) {
             
             case 'View All Departments':
                 viewDepartments()
-            break;
+            break
 
+            case 'View All Roles':
+                viewRoles()
+            break
 
+            case 'View All Employees':
+                viewEmployees()
+            break
+
+            case 'Add a Department':
+                addDepartment()
+            break
+
+            case 'Add a Role':
+                addRole()
+            break
+
+            case 'Add an Employee':
+                addEmployee()
+            break
+
+            case 'Update an Employee Role':
+                updateEmployee()
+            break
         }
     })
 }
 
+// Function that queries the department table (called in the switch case above based from user selection)
 function viewDepartments() {
-    const query = 'SELECT department.id AS id, department.department_name AS name FROM department'
+    const query = 
+        'SELECT department.id AS id, department.department_name AS name FROM department'
     employeeDb.query(query, (error, results) => {
         if (error) {
             throw error
@@ -55,5 +83,24 @@ function viewDepartments() {
     })
 }
 
+// Function that queries the roles table (called in the switch case above based from user selection)
+function viewRoles() {
+    const query = 
+        'SELECT employee_role.title AS title, employee_role.id AS id, employee_role.department_id AS department, employee_role.salary AS salary FROM employee_role JOIN department ON employee_role.department_id = department.id'
+    employeeDb.query(query, (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            console.log('')
+            console.log('Viewing All Roles')
+            console.log('')
+            console.table(results)
+            startTracker()
+        }
+    })
+}
 
+
+
+// Calls function to start
 startTracker()
